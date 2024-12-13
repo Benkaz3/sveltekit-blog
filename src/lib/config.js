13a04e01e8c8884baccf53bf5c9982a9ce43736a
Jinsorei <eeijnl@gmail.com>
2024-12-13 11:40:08 +0700

    changed siteLink to my domain

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,7 +3,7 @@ export const siteDescription =
 	'Gỡ rối kỹ thuật cho marketer từ newbie tới pro! Tại dungtran.me, tôi sẽ giúp bạn làm quen với những kỹ thuật khó nhằn trong marketing – từ tracking, phân tích dữ liệu đến tự động hoá – một cách dễ hiểu và gần gũi.';
 export const siteURL = 'https://www.dungtran.me';
 export const siteImage = `${siteURL}/images/dungtran-og-image.png`;
-export const siteLink = 'https://github.com/josh-collinsworth/sveltekit-blog-starter';
+export const siteLink = 'https://www.dungtran.me';
 export const siteAuthor = '- Dũng Trần | The Marketing Technologist';
 
 export const postsPerPage = 10;
